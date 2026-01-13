420772ee29aa093c66f7ef2223525832a29e5c0c
ZoeRenaudie <143030118+ZoeRenaudie@users.noreply.github.com>
2026-01-12 21:24:49 -0500

    Configure GitHub Pages

--- svelte.config.js
+++ svelte.config.js
@@ -3,39 +3,55 @@ import { mdsvex } from "mdsvex";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeSlug from "rehype-slug";
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Ensures both .svelte and .md files are treated as components (can be imported and used anywhere, or used as pages)
-	extensions: [".svelte", ".md"],
-
-	kit: {
-		adapter: adapter(),
-		prerender: {
-			entries: [
-				"*",
-				"/api/posts/page/*",
-				"/blog/category/*/page/",
-				"/blog/category/*/page/*",
-				"/blog/category/page/",
-				"/blog/category/page/*",
-				"/blog/page/",
-				"/blog/page/*",
-			],
-		},
-	},
-
-	preprocess: [
-		mdsvex({
-			// The default mdsvex extension is .svx; this overrides that.
-			extensions: [".md"],
-
-			// Adds IDs to headings, and anchor links to those IDs. Note: must stay in this order to work.
-			rehypePlugins: [
-				rehypeSlug,
-				rehypeAutolinkHeadings,
-			],
-		}),
-	],
+  extensions: [".svelte", ".md"],
+  
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: undefined,
+      precompress: false,
+      strict: true
+    }),
+    
+    paths: {
+      base: dev ? '' : '/journal-phd'
+    },
+    
+    prerender: {
+      entries: [
+        "*",
+        "/api/posts/page/*",
+        "/blog/category/*/page/",
+        "/blog/category/*/page/*",
+        "/blog/category/page/",
+        "/blog/category/page/*",
+        "/blog/page/",
+        "/blog/page/*",
+      ],
+      handleHttpError: ({ path, referrer, message }) => {
+        // Ignorer les erreurs 404 pour /images/
+        if (path === '/images/') {
+          return;
+        }
+        throw new Error(message);
+      }
+    },
+  },
+  
+  preprocess: [
+    mdsvex({
+      extensions: [".md"],
+      rehypePlugins: [
+        rehypeSlug,
+        rehypeAutolinkHeadings,
+      ],
+    }),
+  ],
 };
 
 export default config;
