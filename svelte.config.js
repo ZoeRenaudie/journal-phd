09d0a2fb61a6accf80efc29d6f9182ab0f00c50c
ZoeRenaudie <143030118+ZoeRenaudie@users.noreply.github.com>
2026-01-13 15:36:25 -0500

    modif svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -19,7 +19,7 @@ const config = {
     }),
     
     paths: {
-      base: dev ? '' : '/journal-phd'
+      base: process.env.NODE_ENV === 'production' ? '/nom-du-repo' : ''
     },
     
     prerender: {
